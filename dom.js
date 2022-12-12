ac7d07ba59ff2d3bec4d1460c58efcada8eb5710
kunalmkv <kunalm4.kvtagore@gmail.com>
2022-12-12 08:20:21 +0530

    task6 in 5

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-console.log(document.all);
+/*console.log(document.all);
 var headerTitle = document.getElementById('header-title');
 headerTitle.style.borderBottom = 'solid 3px #000';
 
@@ -13,4 +13,19 @@ items[3].style.backgroundColor = '#ffff33';
 
 var cla = document.getElementsByClassName('randomClass');
 cla.style.fontWeight = 'bold';
-cla.style.backgroundColor = '#80ffbf';
+cla.style.backgroundColor = '#80ffbf';*/
+
+// var item = document.querySelector('.list-group-item');
+// item[2].style.display = 'none';
+
+/*var items = document.getElementsByClassName('list-group-item');
+for (var i = 0; i < items.length; i++) {
+    items[i].style.display = 'none';
+}*/
+
+var item = document.querySelector('.list-group-item:nth-child(2)');
+item.style.display = 'none';
+var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.backgroundColor = '#f4f4f4';
+}
